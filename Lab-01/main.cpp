911038a763f5e262409a813ebb60392b58e7b7d0
David Hrivna <david.hrivna@seznam.cz>
2022-10-05 17:32:16 +0200

    Added funcionality, only output needs a tweak

--- Lab-01/main.cpp
+++ Lab-01/main.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
 
-void print_subsets(){
-    // { A, B, C, D }
-    // { A, B, C }
-
-}
-
-void int_get_subsets(const char* items, size_t count, bool* signature, int offset = 0){
-    
+void print_subset(const char* items, size_t count, bool* signature ){
+    std::cout << "{ ";
     for (size_t i = 0; i < count; i++){
-        if(!signature[i]){
-            signature[i] = true;
+        if(signature[i]){
+            std::cout << items[i] << " ";
         }
     }
-    
+    std::cout << "}" <<std::endl;
+}
+
+void priv_get_subsets(const char* items, size_t count, bool* signature, size_t offset = 0){
+    print_subset(items, count, signature);
+    for (size_t i = offset; i < count; i++){
+        if(!signature[i]){
+            signature[i] = true;
+            priv_get_subsets(items, count, signature, i);
+        }
+    }
+    signature[offset] = false;
 }
 
 void get_subsets(const char* items, size_t count){
     bool* signature = new bool[count];
-    int_get_subsets(items, count, signature);
+    priv_get_subsets(items, count, signature); 
     delete[] signature;
 }
 
 int main(int argc, char** argv){
     const char items[] = {'A','B','C','D'};
     const size_t count = sizeof(items) / sizeof(items[0]);
+    const size_t subset_count = 1 << count;
     get_subsets(items, count);
 }

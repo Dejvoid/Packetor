8ec09b5a06174c1a53cfa32165923bdef09db5c8
David Hrivna <david.hrivna@seznam.cz>
2022-10-06 00:55:26 +0200

    Reworked subset order

--- Lab-01/main.cpp
+++ Lab-01/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 
-void print_subset(const char* items, size_t count, bool* signature){
+int subset_count(bool* signature, size_t count){
     int ctr = 0;
     for (size_t i = 0; i < count; i++){
-        if(signature[i])
+        if(!signature[i])
             ctr++;
     }
+    return ctr;
+}
+
+void print_subset(const char* items, size_t count, bool* signature){
+    int ctr = subset_count(signature, count);
     std::cout << "{ ";
+    int temp = 0;
     for (size_t i = 0; i < count; i++){
-        if(signature[i]){
+        if(!signature[i]){
             std::cout << items[i];
-            if(i < ctr-1)
+            if(++temp < ctr)
                 std::cout << ",";
             std::cout << " ";
         }
@@ -18,15 +24,17 @@ void print_subset(const char* items, size_t count, bool* signature){
     std::cout << "}" <<std::endl;
 }
 
-void priv_get_subsets(const char* items, size_t count, bool* signature, size_t offset = 0){
-    print_subset(items, count, signature);
-    for (size_t i = offset; i < count; i++){
-        if(!signature[i]){
-            signature[i] = true;
-            priv_get_subsets(items, count, signature, i);
+void priv_get_subsets(const char* items, size_t count, bool* signature, size_t layer = 0){
+    if(++layer <= count){
+        print_subset(items, count, signature);
+        for (size_t i = count-1; i >= 0; i--){
+            if(!signature[i]){
+                signature[i] = true;
+                priv_get_subsets(items, count, signature, layer);
+            }
+            signature[i] = false;
         }
     }
-    signature[offset] = false;
 }
 
 void get_subsets(const char* items, size_t count){

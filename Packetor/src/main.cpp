4688d20ff5e9dfef4f9c8a21fd096fe5ae42a460
hrivnad <david.hrivna@seznam.cz>
2023-04-30 14:05:03 +0200

    Minor changes

--- Packetor/src/main.cpp
+++ Packetor/src/main.cpp
@@ -9,22 +9,7 @@
 #include "arguments.hpp"
 #include "controls.hpp"
 
-//static bool onPacketArrivesBlockingMode(pcpp::RawPacket* packet, pcpp::PcapLiveDevice* dev, void* cookie)
-//{
-//    // extract the stats object form the cookie
-//    Stats* stats = (Stats*)cookie;
-//
-//    // parsed the raw packet
-//    pcpp::Packet parsedPacket(packet);
-//
-//    // collect stats from packet
-//    stats->consumePacket(parsedPacket);
-//
-//    // return false means we don't want to stop capturing after this callback
-//    return false;
-//}
 
-/// @brief 
 int main (int argc, char** argv){ 
     if (argc <= 1) {
         std::cout << "Interactive mode" << std::endl;
@@ -33,20 +18,7 @@ int main (int argc, char** argv){
         return 0;
     }
     else {
-        args_t args{argv + 1, argv + argc};
-        options_t options;
-
+        std::cout << "Argument mode not ready yet, use Interactive mode instead" << std::endl;
+        return 0;
     } 
-
-    //auto device = PcapLiveDeviceList::getInstance().getPcapLiveDevicesList()[1]; 
-    //PacketSender ps{device};
-    ////ps.send_packet();
-    //ps.wifi_deauth();
-//
-    //if (device->open()) {
-    //    Stats stats;
-    //    device->startCaptureBlockingMode(onPacketArrivesBlockingMode, &stats, 10);
-    //    std::cout << "Results:" << std::endl;
-    //    //stats.printToConsole();
-    //}
 } 
